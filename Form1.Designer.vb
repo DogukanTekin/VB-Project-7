73a1ea56fb656e02e9194a1e2b66efda498f58ae
DogukanTekin <95180909+DogukanTekin@users.noreply.github.com>
2022-04-06 02:14:59 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(195, 170)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = "SORU 1"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(337, 12)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(195, 170)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = "SORU 1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(233, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "SORU 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(60, 250)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(311, 244)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1276, 631)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox2.SelectionStart = 0
+        Dim aranacaklar(), aranacak, arananlar() As String
+        Dim sayi, i, j As Integer
+        Dim bos As Boolean = False
+        Dim ayni As Boolean = False
+
+        ' inputla alınacak verinin boş olmaması için döngü kullandım
+        Do While (bos = False)
+            aranacak = InputBox("Aranacak Kelimeleri Giriniz")
+            If aranacak = "" Then
+                MsgBox("Lütfen Boş Bırakmayınız")
+            Else
+                bos = True
+            End If
+        Loop
+
+        'aldığım veriyi boşluğa göre bölerek bir dizi içerisine aktardım
+        'kullanıcı aynı kelimeyi 1'den fazla girmişse eğer fazlalıkları "" olarak değiştirdim
+        aranacaklar = aranacak.Split(" ")
+        Do While (i < aranacaklar.Length)
+            j = i + 1
+            Do While (j < aranacaklar.Length)
+                If String.Compare(aranacaklar(i), aranacaklar(j)) = 0 Then
+                    aranacaklar(i) = ""
+                    j = aranacaklar.Length
+                End If
+                j += 1
+            Loop
+            i += 1
+        Loop
+
+        'richtextbox2 ye 1 i aktarardım ve ardından arananlar dizisine richtextbox2 den split yaptığım verileri ekledim
+        RichTextBox2.Text = RichTextBox1.Text
+        arananlar = RichTextBox2.Text.Split(" ")
+        'arananların ilk elemanıyla aranacakların bütün elemanlarını sıra sıra kontrol ettim. 
+        'arananların ilk elemanı bitince ikinci, üçüncü elemanına devam edecek şekilde algoritma kurdum
+        'boolean değişkenini aynı değer bulup bulmadığını kontrol edebilmek için ekledim
+        For i = 0 To arananlar.Length - 1
+            ' alttaki for döngüsü bittiği zaman j yeniden 0 olarak işleme girebilmesi için j=0 ekledim
+            j = 0
+            ayni = False
+            For j = 0 To aranacaklar.Length - 1
+                ' yukarıda aynı olan değerleri "" ile değiştirmiştim, "" olan değerlerle boşa vakit kaybetmemesi için onlarla işlem yapmadım
+                ' aranacaklar dizisindeki "" olmayan elemanlar karşılaştırmaya tutuldu
+                If aranacaklar(j) <> "" Then
+                    ' eğer arananların i. değeri ile aranacakların j. değeri aynıysa kullanacağım değişkenleri aldım
+                    If String.Compare(arananlar(i), aranacaklar(j)) = 0 Then
+                        sayi = RichTextBox2.Text.Trim().IndexOf(aranacaklar(j).Trim(), sayi)
+                        RichTextBox2.SelectionLength = CInt(aranacaklar(j).Trim().Length)
+                        ayni = True
+                    End If
+                End If
+            Next
+            ' boolean değişkenine göre işlem yaptırdım. Eğer yukarıda aynı kelimeyi bulduysa boolean True değere düştü
+            ' boolean true değere düştüyse eğer renklendirme işlemlerini yaptırdım
+            ' bir sonraki turda renklendirmeye en baştan başlamaması, zaten kontrolünü yapmış olduğu yerleri atlayıp başlaması için döngünün sonunda sayı değişkenini 
+            ' kullanmış olduğum uzunluk kadar yükselttim. 
+            If ayni = True Then
+                RichTextBox2.SelectionStart = sayi
+                RichTextBox2.SelectionColor = Color.White
+                RichTextBox2.SelectionBackColor = Color.Red
+                sayi += RichTextBox2.SelectionLength
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RichTextBox1.Text = ""
+        RichTextBox2.Text = ""
+    End Sub
+End Class
